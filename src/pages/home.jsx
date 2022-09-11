bdfd6d27a2d7d8a1f3940d740d7f8447a02e2343
Kalvin Garcia <plutea.leaf@gmail.com>
2022-09-11 16:17:44 -0500

    updating the home page to keep up with all the additional components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,20 +4,23 @@ import Block from '../components/block';
 import Parallax from '../components/parallax';
 import ImageWithCaption from '../components/image-with-caption';
 import Band from '../components/band';
-import emote from '../assets/images/KalvinEmote.png';
-import space from '../assets/images/space.jpg';
+import FeaturedSection from '../components/featured-section';
+import LinkedPost from '../components/linked-post';
+import me from '../assets/images/me.png';
+import rice from '../assets/images/rice.jpg';
+import spaghetti from '../assets/images/spaghetti.jpg'
 
 export default class Home extends Component {
   render() {
     return (
-      <main>
+      <main style={{paddingTop: '50px'}}>
         <Section className='row' height='800px'>
             <Block className='col-sm'>
-              <Parallax bg_image={space} />
+              <Parallax bg_image={rice} />
             </Block>
             <Block className='col-sm'>
               <ImageWithCaption
-                image={emote}
+                image={me}
                 shape='rounded-rectangle'
                 caption="Welcome to our food blog"
               />
@@ -28,9 +31,16 @@ export default class Home extends Component {
           spacer="rectangle"
           repeat={['true', 4]}
           scroll={['true', 'slow']}
-          colorChange={['false']}
+          colorChange={['true', ['#ff0000', '#00ff00', '#ffff00']]}
         />
-        <Section>
+        <FeaturedSection num={3} />
+        <Section className='row' height='800px'>
+          <Block className='col-sm'>
+            <LinkedPost />
+          </Block>
+          <Block className='col-sm'>
+            <Parallax bg_image={spaghetti}/>
+          </Block>
         </Section>
       </main>
     );
