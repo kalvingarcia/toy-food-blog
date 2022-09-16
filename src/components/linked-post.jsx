62b9909eb2e709f6e34603e33ef644003ff56995
Kalvin Garcia <plutea.leaf@gmail.com>
2022-09-16 16:26:43 -0500

    styling the linked post

--- src/components/linked-post.jsx
+++ src/components/linked-post.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import Section from './section';
 import Block from './block';
 import Parallax from './parallax';
 
@@ -13,22 +12,22 @@ export default class LinkedPost extends Component {
 
   render() {
     return (
-      <Section className='row' height='800px'>
-        <Block className='col-sm'>
-          <Link to="/recipes">
-            <span>Title</span>
-            <div>
-              <span>Click Me the Subtitle</span>
-            </div>
-            <div>
-              <p>Click the description too</p>
-            </div>
-          </Link>
-        </Block>
-        <Block className='col-sm'>
-          <Parallax bg_image={this.props.bg_image}/>
-        </Block>
-      </Section>
+      <Block className={this.generate_class()}>
+        <Parallax className='column-50' bg_image={this.props.bg_image}/>
+        <Link className='column-50 linked-post' to="/recipes">
+          <span className="linked-post-head">Title</span>
+          <div className="post-block">
+            <span className="linked-post-title">Click Me the Subtitle</span>
+            <p className="linked-post-desc">Click the description too</p>
+          </div>
+        </Link>
+      </Block>
     );
+  }
+
+  generate_class() {
+    if(this.props.className)
+      return `row ${this.props.className}`;
+    return "row-600";
   }
 }
