a81ebe2ce92296654afd64e93a2027cb13881884
Kalvin Garcia <plutea.leaf@gmail.com>
2022-09-16 16:23:23 -0500

    updating the navbar to use react-router-dom navlink

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
 
 export default class Navbar extends Component {
   position = {
@@ -19,13 +20,28 @@ export default class Navbar extends Component {
           <span className="brand-logo">Comí Comé</span>
           <ul className="nav-items">
             <li className="nav-link">
-              <a href="/">Home</a>
+              <NavLink
+                exact to="/"
+                style={({isActive}) => ({visibility: isActive ? 'hidden' : '', color: '#222222'})}
+              >
+                Home
+              </NavLink>
             </li>
             <li className="nav-link">
-              <a href="/recipes">Recipes</a>
+              <NavLink
+                to="/recipes"
+                style={({isActive}) => ({color: isActive ? '#000000' : '#222222'})}
+              >
+                Recipes
+              </NavLink>
             </li>
             <li className="nav-link">
-              <a href="/about">About</a>
+              <NavLink
+                to="/about"
+                style={({isActive}) => ({color: isActive ? '#000000' : '#222222'})}
+              >
+                About
+              </NavLink>
             </li>
           </ul>
         </div>
