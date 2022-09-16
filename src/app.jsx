1aba3136e915cd62dcc4e186d7805706746227f4
Kalvin Garcia <plutea.leaf@gmail.com>
2022-09-16 16:16:01 -0500

    converting the app component from a class to a function

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/home';
 import Recipes from './pages/recipes';
 import About from './pages/about';
+import Post from './pages/post';
 import Footer from './components/footer';
 import colors from './assets/utils/colors';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Navbar
-          current={this.current_page}
-          pos='sticky'
-          color={colors.ny_pink}
-        />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
-      </Router>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <Navbar
+        pos='sticky'
+        color={colors.ny_pink}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/recipes/:postID/:postName" element={<Post />} />
+      </Routes>
+      <Footer color={colors.laurel_green} />
+    </Router>
+  );
 }
