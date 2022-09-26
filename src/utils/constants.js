a5dfbf20e27a61270fcd1a7c75fbfd63df698f5d
Kalvin Garcia <plutea.leaf@gmail.com>
2022-09-25 23:29:44 -0500

    mirgrating image assets to the public folder and moving utils

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const COLORS = {
+  laurel_green: '#A9B18F',
+  ny_pink: '#CB807D',
+  cyclamen: '#FE64A3',
+  pink_sherbert: '#F6839C',
+  spanish_pink: '#F0B5B3'
+};
+
+export const ALL_RECIPES_API = "http://localhost:8000/api/recipe/";
+export const FILTER_RECIPES_API = "http://localhost:8000/api/recipe/filter/";
+export const NEWEST_POST_API = "http://localhost:8000/api/recipe/newest"
+export const GET_FEATURED_API = "http://localhost:8000/api/recipe/featured";
+export const RECIPE_BY_ID_API = "http://localhost:8000/api/recipe-id/";
