17415d52f9054241cc38bb417c5605313affda9b
Kalvin Garcia <plutea.leaf@gmail.com>
2022-09-29 18:17:28 -0500

    styling the parallax banner

--- src/components/parallax-banner.jsx
+++ src/components/parallax-banner.jsx
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import Section from './section';
 import Parallax from './parallax';
+import Shader from './shader';
 
 export default class ParallaxBanner extends Component {
   render() {
     return (
-      <Section>
-        <Parallax />
+      <Section className="parallax-banner">
+        <Shader color="black" blendMode="multiply" />
+        <div className="banner-text">
+          <span>{this.props.text}</span>
+        </div>
+        <Parallax className="parallax-image" source={this.props.source} />
       </Section>
     );
   }
